b9abc0a7064cd69d25ffcb5358e9b0ace28a29b5
Daniel Khuu <danieldkhuu@gmail.com>
2022-03-28 23:05:07 -0500

    Import Open File Browser Package
    and replace variables/comments in FileBrowserUpdate.cs

---
+++ Assets/FileBrowserUpdate.cs
@@ -0,0 +1,43 @@
+﻿
+using AnotherFileBrowser.Windows;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class FileBrowserUpdate : MonoBehaviour
+{
+    public RawImage rawImage;
+
+    public void OpenFileBrowser()
+    {
+        var bp = new BrowserProperties();
+        bp.filter = "STL files (*.stl) | *.stl;";
+        bp.filterIndex = 0;
+
+        new FileBrowser().OpenFileBrowser(bp, path =>
+        {
+            //Load stl from local path with UWR
+            StartCoroutine(LoadImage(path));
+        });
+    }
+
+    IEnumerator LoadSTL(string path)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(path))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                var uwrTexture = DownloadHandlerTexture.GetContent(uwr);
+                rawImage.texture = uwrTexture;
+            }
+        }
+    }
+}
